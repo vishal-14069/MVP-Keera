e45aa434b02e2c81f9a573ce7519e7ee6da0e166
neo2wicked <capricorn@Live.com>
2024-07-06 15:15:50 -0500

    updated logic for enter code

--- change-management-app/frontend/src/App.js
+++ change-management-app/frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import AskQuestion from './AskQuestion';
+import EnterCode from './EnterCode';
 import SubmitResponse from './SubmitResponse';
 import './App.css';
 
@@ -9,22 +10,25 @@ function App() {
     <Router>
       <div className="App">
         <header className="App-header">
-          <h1>Change Management Insights</h1>
+          <h1>Decision Loop</h1>
           <nav>
             <ul>
               <li>
                 <Link to="/ask">Ask a Question</Link>
               </li>
               <li>
-                <Link to="/respond">Submit a Response</Link>
+                <Link to="/enter-code">Enter Code</Link>
               </li>
             </ul>
           </nav>
         </header>
-        <Switch>
-          <Route path="/ask" component={AskQuestion} />
-          <Route path="/respond" component={SubmitResponse} />
-        </Switch>
+        <main>
+          <Switch>
+            <Route path="/ask" component={AskQuestion} />
+            <Route path="/enter-code" component={EnterCode} />
+            <Route path="/respond/:code" component={SubmitResponse} />
+          </Switch>
+        </main>
       </div>
     </Router>
   );
