488da49b347f414479fc77cfa53f209a70faccf1
neo2wicked <capricorn@Live.com>
2024-07-07 12:23:16 -0500

    updated enter code logic

--- change-management-app/frontend/src/EnterCode.js
+++ change-management-app/frontend/src/EnterCode.js
@@ -1,17 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 
-function EnterCode({ history }) {
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    history.push('/respond/test');
+class EnterCode extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      code: ''
+    };
+  }
+
+  handleChange = (e) => {
+    this.setState({ code: e.target.value });
   };
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <button type="submit">Submit</button>
-    </form>
-  );
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.history.push(`/submit/${this.state.code}`);
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input
+          type="text"
+          value={this.state.code}
+          onChange={this.handleChange}
+          placeholder="Enter code"
+        />
+        <button type="submit">Submit</button>
+      </form>
+    );
+  }
 }
 
 export default withRouter(EnterCode);
